e83b8ef69e1ac9e7e8b08482429e2a2d862a52dd
Daria-Zakharova <104733899+Daria-Zakharova@users.noreply.github.com>
2022-12-08 10:05:22 +0200

    update file name in import

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import {TbCoffee} from "react-icons/tb";
 import { FeedbackOptions, FeedbackBtn } from "./FeedbackOptions.styled";
-import { getBtnIconColor } from 'utils/getBtnIconColor';
+import { getBtnIconColor } from 'utils/get-btn-icon-color';
 
 export const FeedbackButttons = ({options, onLeaveFeedback}) => {
 
