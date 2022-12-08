5cc284ff8fbc332161def1fec408379b34f0953f
Daria-Zakharova <104733899+Daria-Zakharova@users.noreply.github.com>
2022-12-08 10:04:01 +0200

    edit file name

---
+++ src/utils/get-btn-icon-color.js
@@ -0,0 +1,12 @@
+export const getBtnIconColor = btnValue => {
+    switch (btnValue) {
+        case 'good':
+            return '#3f8125';
+        case 'neutral':
+            return 'orange';
+        case 'bad':
+            return 'red';
+        default:
+            throw new Error('invalid option in Feedback options');
+    }
+}
